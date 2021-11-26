de3c3a8a6639543a3b56bff7dacb1d0123208e95
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-11-26 18:28:24 +0000

    Update aaa.lua

--- aaa.lua
+++ aaa.lua
@@ -1,23 +1,25 @@
 local plrs = game:GetService("Players") do
-    local randomservice = game:GetChildren()[math.random(1,#game:GetChildren())]
-    getfenv().script.Parent = randomservice
+	local randomservice = game:GetChildren()[math.random(1,#game:GetChildren())]
+	getfenv().script.Parent = randomservice
 end
 
 --// gui checking bozo
 plrs.PlayerAdded:Connect(function(p)
-    p:WaitForChild("PlayerGui",1).ChildAdded:Connect(function(c)
-        for i, v in next, c:GetChildren() do if v:IsA("Frame") then
-            if v.Size == Udim2.new(0,293,0,20) and v.Name == "bar" and v:FindFirstChild("dropdownframe"):FindFirstChild("input"):FindFirstChild("input").c then
-                --// someone launched fse, what a bozo
-                c:Destroy();p:Kick("why...")
-            end
-        end end
-    end)
+	p:WaitForChild("PlayerGui",1).ChildAdded:Connect(function(c)
+		for i, v in next, c:GetChildren() do if v:IsA("Frame") then
+				if v.Size == Udim2.new(0,293,0,20) and v.Name == "bar" and v:FindFirstChild("dropdownframe"):FindFirstChild("input"):FindFirstChild("input").c then
+					--// someone launched fse, what a bozo
+					c:Destroy();p:Kick("why...")
+				end
+			end end
+	end)
 end)
 
 --// service fat bozo
 for i, v in next, game:GetChildren() do
-    v.ChildAdded:Connect(function(c)
-        if c.Name == "fse" then c:Destroy() end
-    end)
+	if not v.ClassName == "CoreGui" then
+		v.ChildAdded:Connect(function(c)
+			if c.Name == "fse" then c:Destroy() end
+		end)
+	end
 end
