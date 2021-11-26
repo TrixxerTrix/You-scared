6b74dcc8171962b7193ff62597237bd872bbf0da
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-11-26 18:37:52 +0000

    Update aaa.lua

--- aaa.lua
+++ aaa.lua
@@ -1,26 +1,20 @@
 local fselocations = {"FriendService", "InsertService", "MarketplaceService", "ProximityPromptService"}
 local plrs = game:GetService("Players") do
 	local randomservice = fselocations[math.random(1,#fselocations)]
-	getfenv().script.Parent = randomservice
+	script.Parent = game:WaitForChild(randomservice,1)
 end
 
 --// gui checking bozo
 plrs.PlayerAdded:Connect(function(p)
 	p:WaitForChild("PlayerGui",1).ChildAdded:Connect(function(c)
-		for i, v in next, c:GetChildren() do if v:IsA("Frame") then
-				if v.Size == UDim2.new(0,293,0,20) and v.Name == "bar" and v:FindFirstChild("dropdownframe"):FindFirstChild("input"):FindFirstChild("input").c then
-					--// someone launched fse, what a bozo
-					c:Destroy();p:Kick("why...")
-				end
-			end end
+		if c.Name == "fumo" then p:Kick(); c:Destroy() end
 	end)
 end)
 
 --// service fat bozo
-for i, v in next, game:GetChildren() do
-	if not v.ClassName == "CoreGui" then
-		v.ChildAdded:Connect(function(c)
-			if c.Name == "fse" then c:Destroy() end
-		end)
-	end
+for i, item in next, fselocations do
+	local v = game:WaitForChild(item,1)
+	v.ChildAdded:Connect(function(c)
+		if c.Name == "fse" then c:Destroy() end
+	end)
 end
